f7f5304b8c69d045b3a66d0347b1b5d9478c69d3
Piotr Kasprzyk <ciri@ciri.pl>
2022-01-17 11:02:34 +0100

    Add MultiReplacer

--- sngl_prd.py
+++ sngl_prd.py
@@ -39,6 +39,37 @@ def process_replace(fa, fb, As, Ae, Bs, Be):
     return needs_work
 
 
+class MultiReplacer(object):
+    def __init__(self):
+        '''
+        MultiReplacer:
+        '''
+
+    def process_replace(self, fa, fb, As, Ae, Bs, Be):
+        '''
+        MultiReplacer:
+        '''
+        needs_work = 1
+        delta_a = Ae - As
+        if delta_a == Be - Bs:
+            diff_cnt = 0
+            suspicious_change = 0
+            for i in range(delta_a):
+                part_a = fa[As + i]
+                part_b = fb[Bs + i]
+                if part_a != part_b:
+                    diff_cnt += 1
+                    if both_starts(part_a, part_b, tx_place.sta_8_tement):
+                        pass
+                    elif both_starts(part_a, part_b, tx_place.sta_7_tement):
+                        pass
+                    else:
+                        suspicious_change = 1
+            if diff_cnt <= 2 and not suspicious_change:
+                needs_work = 0
+        return needs_work
+
+
 class TestSinglePeriod(unittest.TestCase):
     def test_equal_length(self):
         '''
